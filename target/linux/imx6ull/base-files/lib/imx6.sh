58e3c96c9986b948e23f3504f69d58e8c23a80da
hejiawencc <hjw0415@outlook.com>
2021-04-13 17:17:34 +0800

    fix:(banner,passwd)同步DoorNet设置 修改banner样式为DoorNet 修改默认登录密码为admin 设置board_name为ebf6ull

--- target/linux/imx6ull/base-files/lib/imx6.sh
+++ target/linux/imx6ull/base-files/lib/imx6.sh
@@ -13,49 +13,8 @@ imx6_board_detect() {
 	machine=$(cat /proc/device-tree/model)
 
 	case "$machine" in
-	"Gateworks Ventana i.MX6 DualLite/Solo GW51XX" |\
-	"Gateworks Ventana i.MX6 Dual/Quad GW51XX")
-		name="gw51xx"
-		;;
-
-	"Gateworks Ventana i.MX6 DualLite/Solo GW52XX" |\
-	"Gateworks Ventana i.MX6 Dual/Quad GW52XX")
-		name="gw52xx"
-		;;
-
-	"Gateworks Ventana i.MX6 DualLite/Solo GW53XX" |\
-	"Gateworks Ventana i.MX6 Dual/Quad GW53XX")
-		name="gw53xx"
-		;;
-
-	"Gateworks Ventana i.MX6 DualLite/Solo GW54XX" |\
-	"Gateworks Ventana i.MX6 Dual/Quad GW54XX" |\
-	"Gateworks Ventana GW5400-A")
-		name="gw54xx"
-		;;
-
-	"Gateworks Ventana i.MX6 Dual/Quad GW551X" |\
-	"Gateworks Ventana i.MX6 DualLite/Solo GW551X")
-		name="gw551x"
-		;;
-
-	"Gateworks Ventana i.MX6 DualLite/Solo GW552X" |\
-	"Gateworks Ventana i.MX6 Dual/Quad GW552X")
-		name="gw552x"
-		;;
-
-	"Gateworks Ventana i.MX6 DualLite/Solo GW553X" |\
-	"Gateworks Ventana i.MX6 Dual/Quad GW553X")
-		name="gw553x"
-		;;
-
-	"Gateworks Ventana i.MX6 DualLite/Solo GW5904" |\
-	"Gateworks Ventana i.MX6 Dual/Quad GW5904")
-		name="gw5904"
-		;;
-
-	"Wandboard i.MX6 Dual Lite Board")
-		name="wandboard"
+	"EmbedFire EBF6Ull Board")
+		name="ebf6ull"
 		;;
 
 	*)
