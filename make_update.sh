046d009bcfe5f8f500211d88370f8d685b877b4c
hejiawen <hjw0415@outlook.com>
2021-05-12 11:38:58 +0800

    feat:kernel 添加r8168驱动,用于RTL8111H

--- make_update.sh
+++ make_update.sh
@@ -33,18 +33,18 @@ case $input in
 esac
 
 
-echo '************************config set info *******************'
+echo '**********************config set info *********************'
 echo 'DEVICE_NAME=' $DEVICE_NAME
 echo 'DEVICE_CFG =' $DEVICE_CFG
 
 
-echo '**************beginning update and install feeds***********'
+echo '**************begining update and install feeds************'
 #更新升级软件包列表
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 
 
-echo '**************application DoorNet1 defconfig***************'
+echo '**************application' ${DEVICE_NAME} 'defconfig***************'
 #使用DoorNet1默认配置
 cat ./config/$DEVICE_CFG > .config
 make defconfig
@@ -52,7 +52,7 @@ make defconfig
 
 echo '**************download all packages***********************'
 #下载软件包
-# make download V=s
+make download V=s
 
 ###如果自己在menuconfig中修改了配置，
 ###使用#把使用默认配置的命令注释掉再执行此脚本可一键更新软件包
